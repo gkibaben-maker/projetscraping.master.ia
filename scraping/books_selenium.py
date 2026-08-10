a2306393f641878e293d92d7c1905850826202b4
gkibaben-maker <gkibaben@gmail.com>
2026-08-10 14:52:42 +0200

    Ajout configuration Selenium

--- scraping/books_selenium.py
+++ scraping/books_selenium.py
@@ -1,1 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
+driver = webdriver.Chrome(options=options)
